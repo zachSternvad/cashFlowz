ca1937135a6fb8eba71bc4d85739b5da8e20c541
Zachery <zachery.meck@gmail.com>
2025-04-13 13:26:08 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace cashFlowz
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
