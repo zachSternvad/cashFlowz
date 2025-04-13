0d9b7312f32cfdca7e1220e45b49c055935bf2d7
Zachery <zachery.meck@gmail.com>
2025-04-13 15:37:12 +0200

    Started on the MainViewModel file.

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.ObjectModel; // Crucial for UI updates on collection changes
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Input; // For ICommand
+using cashFlowz.Models;
+using CommunityToolkit.Mvvm.ComponentModel; // From NuGet package
+using CommunityToolkit.Mvvm.Input; // For RelayCommand
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace cashFlowz.ViewModels
+{
+    public partial class MainViewModel : ObservableObject // Inherit from ObservableObject
+    {
+        // -- Properties for Data Binding --
+
+        // ObservableCollection automatically notifies the UI when items are added/removed
+        [ObservableProperty] // Generates property boilerplate
+        private ObservableCollection<Transaction> _transactions = new ObservableCollection<Transaction>();
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(FormattedBalance))] // Update FormattedBalance when balance changes
+        private decimal _balance;
+
+        // Properties for the input form
+        [ObservableProperty]
+        [ObservableProperty]
+        [ObservableProperty]
+        [ObservableProperty]
+
+        public string FormattedBalance => $"Balance: {Balance:C}";
+    }
+}
